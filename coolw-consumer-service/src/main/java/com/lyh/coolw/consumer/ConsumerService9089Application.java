0431666a6e518363a7bb6fe1834fa7734561c056
longwei <longwei@digilink-tech.com>
2021-02-26 15:55:17 +0800

    turbine

---
+++ coolw-consumer-service/src/main/java/com/lyh/coolw/consumer/ConsumerService9089Application.java
@@ -0,0 +1,22 @@
+package com.lyh.coolw.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * "@EnableEurekaClient"：Eureka注册中心，推荐使用
+ * "@EnableDiscoveryClient"：其他注册中心，推荐使用
+ * "@EnableCircuitBreaker":断路器
+ */
+@EnableCircuitBreaker
+@EnableEurekaClient
+@SpringBootApplication
+public class ConsumerService9089Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerService9089Application.class, args);
+    }
+
+}
---
+++ coolw-hystrix-turbine/src/test/java/com/lyh/coolw/hystrix/turbine/HystrixTurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyh.coolw.hystrix.turbine;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixTurbineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
